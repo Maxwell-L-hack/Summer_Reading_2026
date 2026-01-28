ae947e28066799c8b8f2e1309e0bd765b78c1439
TWatkins27 <teaganw@saintpetermn.gov>
2026-01-28 17:24:32 -0600

    Edited main.js styles.css

--- main.js
+++ main.js
@@ -3,6 +3,19 @@ window.addEventListner('load'.function() {
     const ctx = canvas.getContext('2d');
     canvas.width = 500;
     canvas.height = 500;
+
+    class Game {
+    constructor(width, height) {
+            this.width = width;
+            this.height = height;
+        }
+    update() {
+
+    }
+    draw() {
+
+    }
+    }
 });
 
 //Scientist spritesheet
