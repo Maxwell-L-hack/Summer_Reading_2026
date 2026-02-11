a0e60955c05f98eaf4c80e6d131362c12eb8cd8f
Maxwell-L-hack <maxwelll@hackclub.app>
2026-02-11 17:02:48 -0600

    Maybe fixed it

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const dnaInput = document.getElementById("dna_code");
-    
-    dnaInput.addEventListener("keypress", function(event) {
-        if (event.key === "Enter") {
-            let dnaCodeString = dnaInput.value;
-            console.log(dnaCodeString);
-        }
-    });
+const dnaInput = document.getElementById("dna_code");
+
+dnaInput.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        let dnaCodeString = dnaInput.value;
+        console.log(dnaCodeString);
+    }
 });
