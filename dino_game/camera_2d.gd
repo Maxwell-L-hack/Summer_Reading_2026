18c8465d90175a63b45addbeec3c70f6ac6933e0
Maxwell Lenhoff <papalenhoff@gmail.com>
2026-02-21 11:48:24 -0600

    Made the beginning of the dino game

---
+++ dino_game/camera_2d.gd
@@ -0,0 +1,7 @@
+extends Camera2D
+
+@onready var player = $".."
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	global_position = player.global_position
