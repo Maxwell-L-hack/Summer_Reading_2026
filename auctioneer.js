d3abc8fd1fcd41ff98332bd6d54f867f2823295c
TWatkins27 <teaganw@saintpetermn.gov>
2026-03-18 17:56:41 -0500

    Added styling

--- auctioneer.js
+++ auctioneer.js
@@ -4,31 +4,37 @@ const moneyAmount = document.getElementById("money amount");
 const bidder = document.getElementById("bidder");
 var money = 0;
 
-function add() {
-    money += 100;
-    console.log("money added");
+function updateBidder() {
     if (money) moneyAmount.textContent = `$${money}`;
+
     switch (money) {
         case 0:
             bidder.textContent = "There are no bidders.";
             break;
-        case 100:
+        case 1000:
             bidder.textContent = "There are three bidders.";
             break;
-        case 500:
+        case 5000:
             bidder.textContent = "There are two bidders.";
             break;
-        case 1000:
-            bidder.textContent = "There is one bidder.";
+        case 10000:
+            bidder.textContent = "There is one bidder.  This bidder is the Corporate Corporation, a group known for research and questionable morals.";
             break;
     }
 }
 
+function add() {
+    money += 1000;
+    console.log("money added");
+    updateBidder();
+}
+
 
 function subtract() {
-    money -= 100;
+    money -= 1000;
     console.log("money subtracted");
-    if (money) moneyAmount.textContent = `$${money}`;
+    updateBidder();
 };
 less.onclick = subtract;
 more.onclick = add;
+
