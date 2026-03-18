9e121b030387a06a5cbf9b93a4476bd55f85e6a7
TWatkins27 <teaganw@saintpetermn.gov>
2026-03-18 17:39:08 -0500

    Added button logic to auctioneer.js

--- auctioneer.js
+++ auctioneer.js
@@ -1,15 +1,34 @@
 const more = document.getElementById("more");
 const less = document.getElementById("less");
+const moneyAmount = document.getElementById("money amount");
+const bidder = document.getElementById("bidder");
 var money = 0;
 
 function add() {
     money += 100;
+    console.log("money added");
+    if (money) moneyAmount.textContent = `$${money}`;
+    switch (money) {
+        case 0:
+            bidder.textContent = "There are no bidders.";
+            break;
+        case 100:
+            bidder.textContent = "There are three bidders.";
+            break;
+        case 500:
+            bidder.textContent = "There are two bidders.";
+            break;
+        case 1000:
+            bidder.textContent = "There is one bidder.";
+            break;
+    }
 }
 
-more.onclick = add;
 
 function subtract() {
     money -= 100;
-}
-
-more.onclick = subtract;
+    console.log("money subtracted");
+    if (money) moneyAmount.textContent = `$${money}`;
+};
+less.onclick = subtract;
+more.onclick = add;
