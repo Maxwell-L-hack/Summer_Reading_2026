a345504f0b800ba3b50192d315819c0c4d3fdb39
Maxwell Lenhoff <maxwelll@hackclub.app>
2026-05-06 17:55:54 -0500

    added password check using a placeholder password

--- final.js
+++ final.js
@@ -3,3 +3,22 @@ function changeToDesktop() {
 	document.getElementById('desktop').style.display = 'flex';
 	location.hash = 'congratulations_dialog';
 }
+
+const finalPasswordInput = document.getElementById("final_password");
+let finalPassword;
+
+finalPasswordInput.addEventListener("keypress", function(event) {
+	if (event.key === "Enter") {
+		finalPassword = finalPasswordInput.value;
+		finalPasswordCheck(finalPassword);
+	} else if ("1" === "1") {
+		// add in login button password check
+	}
+});
+
+function finalPasswordCheck(finalPasswordString) {
+	console.log(finalPasswordString)
+	if (finalPasswordString === "secret code") {
+		changeToDesktop();
+	}
+}
