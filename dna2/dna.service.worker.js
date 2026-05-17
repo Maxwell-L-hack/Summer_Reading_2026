370c110f5902ea36bbe2fc61956b18b23f99b1b6
Maxwell Lenhoff <papalenhoff@gmail.com>
2026-05-17 15:14:11 -0500

    Changed boot splash

--- dna2/dna.service.worker.js
+++ dna2/dna.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779047468|90939953';
+const CACHE_VERSION = '1779047740|363030427';
 /** @type {string} */
 const CACHE_PREFIX = 'dna2-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
