e1a15820e5dc13a4cdbe6d48c3106799a0812730
Maxwell Lenhoff <maxwelll@hackclub.app>
2026-06-05 17:56:52 -0500

    Exported the updated dna game

--- dna1/dna.service.worker.js
+++ dna1/dna.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1779048285|243423461';
+const CACHE_VERSION = '1780700103|85791543';
 /** @type {string} */
 const CACHE_PREFIX = 'dna1-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
